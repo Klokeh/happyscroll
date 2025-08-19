fd78db807fd8fcbfdb7e7fcf974f64f20b16113e
Klokeh <david@wishplay.ca>
2025-08-18 23:03:34 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ let currentPost = null;
 
 async function loadPosts() {
   console.log("🟢 loadPosts() called");
-  const res = await fetch('/api/shuffle');
+  const res = await fetch('https://happyscroll.onrender.com/api/shuffle');
   const data = await res.json();
   console.log("🟢 Data from backend:", data);
 
@@ -25,14 +25,14 @@ function displayPost(post) {
 
 // Next / Previous buttons
 async function nextPost() {
-  const res = await fetch('/api/next');
+  const res = await fetch('https://happyscroll.onrender.com/api/next');
   const data = await res.json();
   currentPost = data.post;
   displayPost(currentPost);
 }
 
 async function prevPost() {
-  const res = await fetch('/api/prev');
+  const res = await fetch('https://happyscroll.onrender.com/api/prev');
   const data = await res.json();
   currentPost = data.post;
   displayPost(currentPost);
